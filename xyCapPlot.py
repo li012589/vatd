21877f7a950e5c97e1911105f3574411de6db590
lish <contactlish@gmail.com>
2024-02-06 18:00:49 +0800

    add training and plot scripts

---
+++ xyCapPlot.py
@@ -0,0 +1,249 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from flow import TpwLinearSpline
+import source, flow, utils
+import h5py, os
+import utils
+
+
+def heatCapPlot(model, L, batchSize, target, betaMin=0.5, betaMax=1.5, stepNum=100, savePath=None, show=False, autodiff=True, repAcptRej=False, autodiffLevel=0, device=torch.device('cpu')):
+    if savePath is None:
+        savePath="./"
+    if autodiffLevel == 0:
+        savePath += "_level0"
+    else:
+        savePath += "_level1"
+    if not os.path.exists(savePath):
+        os.makedirs(savePath)
+
+    lnZlst = []
+    lnZerrlst = []
+    heatClst = []
+    meanElst = []
+
+    if autodiff or repAcptRej:
+        dlogPlst = []
+
+    if autodiff:
+        deltaMeanlst = []
+
+    betaLst = torch.linspace(betaMin, betaMax, stepNum).to(device)
+    xy = target
+
+    for beta in betaLst:
+        print(beta.item())
+        if repAcptRej:
+            beta = beta.detach().repeat(batchSize, 1).requires_grad_(True).to(device)
+            T = 1/beta
+
+            z = model.prior.sample(batchSize, T=T)
+            if autodiffLevel == 1:
+                z = z.detach()
+            zlogProb = model.prior.logProbability(z, T=T)
+            samples, logDet = model.forward(z, T=T)
+            logProb = zlogProb - logDet
+            energyWithT = xy.energyWithT(samples, T=T)
+            logZ = -(logProb + energyWithT)
+
+            dlogP = torch.autograd.grad(logProb, beta, grad_outputs=torch.ones(batchSize, 1).to(device), create_graph=True, retain_graph=True)[0]
+            dEdT = torch.autograd.grad(energyWithT, beta, grad_outputs=torch.ones(batchSize, 1).to(device), create_graph=True, retain_graph=True)[0]
+
+            dlogZ = -dlogP - dEdT
+
+            if autodiffLevel == 0:
+                logQdivR = model.prior.sourceList[1].logAceptRej()
+
+                dlogQdivR = torch.autograd.grad(logQdivR, beta, grad_outputs=torch.ones(batchSize, 1).to(device), create_graph=True)[0]
+
+                with torch.no_grad():
+                    B2 = (logZ * (dlogQdivR**2)).mean() / (dlogQdivR**2).mean()
+                    B3 = (logProb * (dlogQdivR**2)).mean() / (dlogQdivR**2).mean()
+
+                    partFn = logZ.mean()
+                    partFnErr = logZ.std()
+                    meanE = -(dlogZ + (logZ - B2) * dlogQdivR).mean()
+                    dlogPest = (dlogP + (logProb - B3) * dlogQdivR).mean()
+
+            if autodiffLevel == 1:
+                dzlogProb = torch.autograd.grad(zlogProb, beta, grad_outputs=torch.ones(batchSize, 1).to(device), create_graph=True, retain_graph=True)[0]
+
+                with torch.no_grad():
+                    B2 = (logZ * (dzlogProb**2)).mean() / (dzlogProb**2).mean()
+                    B3 = (logProb * (dzlogProb**2)).mean() / (dzlogProb**2).mean()
+
+                    partFn = logZ.mean()
+                    partFnErr = logZ.std()
+                    meanE = -(dlogZ + (logZ - B2) * dzlogProb).mean()
+                    dlogPest = (dlogP + (logProb - B3) * dzlogProb).mean()
+
+            lnZlst.append(partFn.item()/L**2)
+            lnZerrlst.append(partFnErr.item()/L**2)
+            meanElst.append(meanE.item()/L**2)
+
+            dlogPlst.append(dlogP.mean().item()/L**2)
+            heatClst.append((beta[0] * dlogPest).item()/L**2)
+
+        elif autodiff:
+            beta = beta.detach().requires_grad_(True).to(device)
+
+            z = model.prior.sample(batchSize, T=1/beta)
+            z = z.detach()
+            zlogProb = model.prior.logProbability(z, T=1/beta)
+            samples, logDet = model.forward(z, T=1/beta)
+            logProb = zlogProb - logDet
+            energyWithT = xy.energyWithT(samples, T=1/beta)
+            energy = xy.energyWithT(samples)
+
+            dlogP = torch.autograd.grad(logProb.mean(), beta, create_graph=True, retain_graph=True)[0]
+
+            dEdT = torch.autograd.grad(energyWithT.mean(), beta, create_graph=True, retain_graph=True)[0]
+
+            loss = logProb + energyWithT
+            logZ = -(loss).mean()
+            dlogZ = -dlogP - dEdT
+
+            deltaMean = -dlogZ - energy.mean()
+
+            with torch.no_grad():
+                partFn = logZ
+                partFnErr = loss.std()
+                meanE = -dlogZ
+
+            lnZlst.append(partFn.item()/L**2)
+            lnZerrlst.append(partFnErr.item()/L**2)
+            meanElst.append(meanE.item()/L**2)
+            deltaMeanlst.append(deltaMean.item()/L**2)
+
+            dlogPlst.append(dlogP.item()/L**2)
+            heatClst.append((beta * dlogP.mean()).item()/L**2)
+
+        else:
+            # direct sample and statistical
+            beta = beta.detach()
+            T = 1/beta
+
+            with torch.no_grad():
+                z = model.prior.sample(batchSize, T=T)
+                zlogProb = model.prior.logProbability(z, T=T)
+                samples, logDet = model.forward(z, T=T)
+                logProb = zlogProb - logDet
+                energy = xy.energy(samples)
+                logZ = -(logProb + energy * beta)
+                partFnErr = logZ.std()
+                logZ = logZ.mean()
+            meanE = energy.mean() / L**2
+            Cv = beta**2 * ((energy**2).mean() - (energy.mean())**2) / L**2
+
+            lnZlst.append(logZ.item()/L**2)
+            lnZerrlst.append(partFnErr.item()/L**2)
+            meanElst.append(meanE.item())
+            heatClst.append(Cv.item())
+
+    lnZlst = np.array(lnZlst)
+    meanElst = np.array(meanElst)
+    heatClst = np.array(heatClst)
+
+    if autodiff or repAcptRej:
+        dlogPlst = np.array(dlogPlst)
+
+    lnZerrlst = np.array(lnZerrlst)
+    if autodiff:
+        deltaMeanlst = np.array(deltaMeanlst)
+
+    betaLst = betaLst.cpu().numpy()
+
+    with open(os.path.join(savePath, "xyplotdatalite.npy"), "wb") as f:
+        np.save(f, betaLst)
+        np.save(f, lnZlst)
+        np.save(f, meanElst)
+        np.save(f, heatClst)
+        np.save(f, dlogPlst)
+
+    plt.figure(figsize=(12, 9))
+    plt.title('lnZ')
+    plt.xlabel('beta')
+    plt.ylabel('lnZ')
+    plt.errorbar(betaLst, lnZlst, yerr=lnZerrlst, marker='o', label="est.")
+    plt.legend(loc='best')
+    plt.savefig(os.path.join(savePath, 'xylnZ.pdf'))
+
+    plt.figure(figsize=(12, 9))
+    plt.title('Mean Energy')
+    plt.xlabel('beta')
+    plt.ylabel('Mean Energy')
+    plt.scatter(betaLst, meanElst, marker='o', label="est.")
+    plt.legend(loc='best')
+    plt.savefig(os.path.join(savePath, 'xymeanE.pdf'))
+
+    if autodiff or repAcptRej:
+        plt.figure(figsize=(12, 9))
+        plt.title('dlogP')
+        plt.xlabel('beta')
+        plt.ylabel('dlogP')
+        plt.scatter(betaLst, dlogPlst, marker='o', label="est.")
+        plt.legend(loc='best')
+        plt.savefig(os.path.join(savePath, 'dlogP.pdf'))
+
+    plt.figure(figsize=(12, 9))
+    plt.title('Heat Capacity')
+    plt.xlabel('beta')
+    plt.ylabel('Heat Capacity')
+    plt.scatter(betaLst, heatClst, marker='o', label="est.")
+    plt.legend(loc='best')
+    plt.savefig(os.path.join(savePath, 'xyheatC.pdf'))
+
+    if autodiff:
+        plt.figure(figsize=(12, 9))
+        plt.title('deltaMean')
+        plt.xlabel('beta')
+        plt.ylabel('dlogZ')
+        plt.scatter(betaLst, deltaMeanlst, marker='o', label="est.")
+        plt.legend(loc='best')
+        plt.savefig(os.path.join(savePath, 'deltaMean.pdf'))
+
+    if show:
+        plt.show()
+
+
+if __name__ == "__main__":
+    #torch.manual_seed(42)
+    # parameters for dists
+    L = 16  # lattice length
+    num = 300 # plot points number
+    batchSize = 1000 # the batch size to estimate free energy
+
+    device = torch.device("cpu")
+    #device = torch.device("cuda:1")
+
+    loadPath = "./opt/uniformSoftplusCNN_testcubic_vonMises_Multi_8_XY_J1.0_T1.0_L8_t_0.5_0.75_0.85_0.9_1.0_1.1_1.25_2.4/"
+
+    model = torch.load(os.path.join(loadPath, "best_TrainLoss_joint.saving"), map_location=device)
+
+    T0 = 1. # base t for XY dist
+    e = -1
+    trainTime = -1
+    factorLst = [0.5, 0.75, 0.85, 0.9, 1., 1.1, 1.25, 2.4] # multi factors for T0 to form temperatures points
+    Tlist = [term * T0 for term in factorLst]
+    target = source.XYbreakSym(L=L, d=2, T=T0, J=1.0, flg2d=True, fixFirst=0).to(device)
+    lossLst = []
+    for t in Tlist:
+        with torch.no_grad():
+            T = torch.tensor(t).to(device)
+            samples, logProb = model.sample(batchSize, T=T)
+            loss = (logProb + target.energyWithT(samples, T=T)).mean(dim=0, keepdim=True)
+        lossLst.append(loss.detach().item())
+
+    lossLst = np.array(lossLst)
+    lossSum = lossLst.sum()
+    printString = "epoch: %d, L: %.5f, "
+    resultLst = [e, lossSum]
+    for idx, factor in enumerate(factorLst):
+        printString += "XY@" + str(factor) + ": %.2f, "
+        resultLst += [lossLst[idx].item()]
+    printString += "time: %.2f"
+    resultLst += [trainTime]
+    resultLst = tuple(resultLst)
+    print(printString % resultLst)
+
+    heatCapPlot(model, L, batchSize, target, stepNum=num, show=True, savePath=os.path.join(loadPath, "pic"), autodiff=False, repAcptRej=True, autodiffLevel=1, device=device)
---
+++ xyVonMcubic.py
@@ -0,0 +1,244 @@
+import time
+import torch
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+import source, flow, utils
+import h5py, os
+
+from decoratedResnet import DecoratedResNet, SimpleMaskedMLPforSplineFlow
+
+#torch.manual_seed(42)
+
+# loading obej
+load = None
+
+autodiffLevel = 0 # 0 for rejAcptRep; 1 for priorReinforce; None direct repam
+
+# parameters for XY dists
+L = 16 # lattice length
+T0 = 1. # base t for XY dist
+J = 1. # interacting factor
+factorLst = [0.5, 0.75, 0.85, 0.9, 1., 1.1, 1.25, 2.4] # multi factors for T0 to form temperatures points
+Tlist = [term * T0 for term in factorLst]
+
+# parameters for the beta range
+betalow = 0.4
+betahigh = 2.0
+
+betaRange = betahigh - betalow
+
+stepNum = 35
+
+# parameters for cubic spline flow
+K = 25 # the number of bins
+pwLinearNnet = 16 # inner t net layer number
+# for usage of MLP
+pwLinearNetVector = [400, 500, 600] # t net hidden layer shape
+
+# to fix the first spin
+fixFirst = torch.tensor([[0.0]])
+
+# for usage of resnet
+kernelSize = 9
+hiddenChannels = 128
+hiddenConvLayers = 6
+hiddenWidth = 128
+hiddenFcLayers = 1
+
+mappingBoundary = None
+B = 1 # boundary parameter
+
+if B != np.pi:
+    mappingBoundary = np.pi / B
+
+# parameters for optimization
+lr = 7.e-4 # learning rate
+eps = 1.e-8
+lrdecay = 0.997
+warmup = 500
+batchSize = 1024 # training batch size
+evalBatchSize = 2000 # evalutaion batch size
+maxIter = 10000 # max step number
+saveStep = 50 # save every n steps
+clipGrad = 0.0
+lamd1 = 5.0
+
+# define computation device
+device = torch.device("cpu")
+#device = torch.device("cuda:1")
+
+# create saving folder
+name = "cubic_vonMises_Multi_" + str(len(factorLst)) + "_"+"XY_J"+str(J) + "_T"+str(T0)+"_L"+str(L)+"_t_"+str(factorLst).replace(', ','_').replace('[','').replace(']','')
+rootFolder = os.path.join("opt", name)
+utils.createWorkSpace(rootFolder)
+
+# init XY at different temperatures
+target = source.XYbreakSym(L=L, d=2, T=T0, J=J, flg2d=True, fixFirst=fixFirst.item()).to(device)
+
+# prior parameters mapper
+kappaMapper = utils.SoftplusWithBound(lowerBound=1e-3) # map kappa to lower bound if it's too low
+
+if load is None:
+    # init prior
+    p1 = source.DeltaSource([1, 1], pos=fixFirst.view(-1))
+    p2 = source.VonMisesImp([1, L**2 - 1], mu=0.1 * torch.randn([1, L * L - 1]), kappa=(torch.abs(torch.ones([1, L * L -1]) + 0.1 * torch.randn([1, L * L - 1]))), B=B, trainMu=True, trainKappa=True, kappaMapper=kappaMapper)
+    prior = source.ConcatenatedSource([p1, p2], dim=-1, reshape=[1, L, L], directLogProb=True)
+
+    # init cubice spline
+    maskList = []
+    netList = []
+    # checkboard
+    for n in range(pwLinearNnet // 3):
+        if n % 2 == 0:
+            b = torch.zeros(1, L//2, L//2, 2, 2)
+            b[:, :, :, 0, 0] = 1
+            b[:, :, :, 1, 1] = 1
+            b = b.permute([0, 1, 3, 2, 4]).reshape(1, L, L)
+            netList.append(DecoratedResNet(L, 2, kernelSize, 2 * K + 2, hiddenChannels, hiddenConvLayers, hiddenWidth, hiddenFcLayers, activation=nn.ELU()))
+        else:
+            b = 1 - b
+            netList.append(DecoratedResNet(L, 2, kernelSize, 2 * K + 2, hiddenChannels, hiddenConvLayers, hiddenWidth, hiddenFcLayers, activation=nn.ELU()))
+        maskList.append(b)
+
+    maskList = torch.cat(maskList, 0).to(torch.uint8)
+    tpwLinear = flow.CubicSpline(netList, maskList, K=K, B=B, fixFirst=fixFirst)
+
+    joint = flow.CombinedFlow([tpwLinear], prior=prior, mappingBoundary=mappingBoundary).to(device)
+
+    bestTrainLoss = 99999999
+else:
+    joint = torch.load(load, map_location=device)
+    print("Model loaded at:", load)
+
+    # evaluation loaded model
+    lossLst = []
+    for t in Tlist:
+        with torch.no_grad():
+            T = torch.tensor(t).to(device)
+            samples, logProb = joint.sample(evalBatchSize, T=T)
+            loss = (logProb + target.energyWithT(samples, T=T)).mean(dim=0, keepdim=True)
+        lossLst.append(loss.detach().item())
+
+    lossLst = np.array(lossLst)
+    lossSum = lossLst.sum()
+
+    bestTrainLoss = lossSum
+
+    # feeddback
+    printString = "Loaded model, L: %.5f, "
+    resultLst = [lossSum]
+    for idx, factor in enumerate(factorLst):
+        printString += "XY@" + str(factor) + ": %.2f, "
+        resultLst += [lossLst[idx].item()]
+    resultLst = tuple(resultLst)
+    print(printString % resultLst)
+
+params = list(joint.parameters())
+params = list(filter(lambda p: p.requires_grad, params))
+nparams = sum([np.prod(p.size()) for p in params])
+print('total nubmer of trainable parameters:', nparams)
+
+# init optimizer
+optimizer = torch.optim.Adamax(params, lr=lr)
+
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 1000, gamma = 0.8)
+
+# start optimize
+LOSS = []
+for e in range(maxIter):
+    try:
+        tstart = time.time()
+        # training
+        for s in range(stepNum):
+            optimizer.zero_grad()
+            beta = (torch.rand(batchSize) * betaRange + betalow).to(device)
+
+            z = joint.prior.sample(batchSize, T=1/beta)
+            if autodiffLevel == 1:
+                z = z.detach()
+            zlogProb = joint.prior.logProbability(z, T=1/beta)
+            samples, logDet = joint.forward(z, T=1/beta)
+            logProb = zlogProb - logDet
+
+            loss = (logProb + target.energyWithT(samples, T=1/beta))
+
+            if autodiffLevel == 0:
+                gCorr = (loss.detach() - loss.detach().mean()) * joint.prior.sourceList[1].logAceptRej()
+            elif autodiffLevel == 1:
+                gCorr = (loss.detach() - loss.detach().mean()) * zlogProb
+            elif autodiffLevel is None:
+                gCorr = 0
+            lossReinforce = loss + gCorr
+            lossReinforce = loss.mean(0, keepdim=True)
+
+            lossReinforce.backward()
+
+            if clipGrad != 0:
+                nn.utils.clip_grad_norm_(params, clipGrad)
+
+            optimizer.step()
+            # ramp up the kappa if it gets too small to get gradients
+            joint.prior.sourceList[1].kappa.data[torch.where(joint.prior.sourceList[1].kappa<=4e-4)] = 4.5e-4
+
+        trainTime = time.time() - tstart
+
+        # evaluation
+        lossLst = []
+        for t in Tlist:
+            with torch.no_grad():
+                T = torch.tensor(t).to(device)
+                samples, logProb = joint.sample(evalBatchSize, T=T)
+                loss = (logProb + target.energyWithT(samples, T=T)).mean(dim=0, keepdim=True)
+            lossLst.append(loss.detach().item())
+
+    except Exception as err:
+        torch.save(joint, os.path.join(rootFolder, 'crush_saved.saving'))
+        print(err)
+        import pdb
+        pdb.set_trace()
+
+    lossLst = np.array(lossLst)
+    lossSum = lossLst.sum()
+
+    LOSS.append(lossSum)
+    if lossSum < bestTrainLoss:
+        bestTrainLoss = lossSum.item()
+        torch.save(joint, os.path.join(rootFolder, 'best_TrainLoss_joint.saving'))
+        print("--> Updated best model: ", bestTrainLoss)
+
+    # feeddback
+    printString = "epoch: %d, L: %.5f, "
+    resultLst = [e, lossSum]
+    for idx, factor in enumerate(factorLst):
+        printString += "XY@" + str(factor) + ": %.2f,"
+        resultLst += [lossLst[idx].item()]
+    printString += "time: %.2f, best: %.5f"
+    resultLst += [trainTime, bestTrainLoss]
+    resultLst = tuple(resultLst)
+    print(printString % resultLst)
+
+    # step the schedular
+    scheduler.step()
+
+    # save
+    if e % saveStep == 0 or e == 0:
+        # save joint and opt
+        torch.save(joint, os.path.join(rootFolder, 'savings', joint.name + "_epoch_" + str(e) + ".saving"))
+        torch.save(optimizer, rootFolder + 'savings/' + joint.name + "_epoch_" + str(e) + "_opt.saving")
+        # save loss values
+        with h5py.File(os.path.join(rootFolder, "records", "LOSS"+'.hdf5'), 'w') as f:
+            f.create_dataset("LOSS", data=np.array(LOSS))
+        # plot loss curve
+        lossfig = plt.figure(figsize=(8, 5))
+        lossax = lossfig.add_subplot(111)
+        epoch = len(LOSS)
+        lossax.plot(np.arange(epoch), np.array(LOSS), 'go-', label="loss", markersize=2.5)
+        lossax.set_xlim(0, epoch)
+        lossax.legend()
+        lossax.set_title("Loss Curve")
+        plt.savefig(os.path.join(rootFolder, 'pic', 'lossCurve.png'), bbox_inches="tight", pad_inches=0)
+        plt.close()
+        # clean extra saving
+        utils.cleanSaving(rootFolder, e, 6 * saveStep, joint.name)
