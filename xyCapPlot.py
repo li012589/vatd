d121b6cec5419b01e4f88cbec6ac88167859c8e9
lish <contactlish@gmail.com>
2024-02-06 18:28:28 +0800

    add mini batch

--- xyCapPlot.py
+++ xyCapPlot.py
@@ -7,15 +7,19 @@ import h5py, os
 import utils
 
 
-def heatCapPlot(model, L, batchSize, target, betaMin=0.5, betaMax=1.5, stepNum=100, savePath=None, show=False, autodiff=True, repAcptRej=False, autodiffLevel=0, device=torch.device('cpu')):
+def heatCapPlot(model, L, batchSize, target, smallBatchNum=1, betaMin=0.5, betaMax=1.5, stepNum=100, savePath=None, show=False, autodiff=True, repAcptRej=False, autodiffLevel=0, device=torch.device('cpu')):
     if savePath is None:
         savePath="./"
     if autodiffLevel == 0:
         savePath += "_level0"
-    else:
+    elif autodiffLevel == 1:
         savePath += "_level1"
+    else:
+        savePath += "_stat"
     if not os.path.exists(savePath):
         os.makedirs(savePath)
+
+    smallBatch = batchSize // smallBatchNum
 
     lnZlst = []
     lnZerrlst = []
@@ -33,123 +37,124 @@ def heatCapPlot(model, L, batchSize, target, betaMin=0.5, betaMax=1.5, stepNum=1
 
     for beta in betaLst:
         print(beta.item())
-        if repAcptRej:
-            beta = beta.detach().repeat(batchSize, 1).requires_grad_(True).to(device)
-            T = 1/beta
+        for _b in range(smallBatchNum):
+            if repAcptRej:
+                _beta = beta.detach().repeat(smallBatch, 1).requires_grad_(True).to(device)
+                T = 1/_beta
 
-            z = model.prior.sample(batchSize, T=T)
-            if autodiffLevel == 1:
-                z = z.detach()
-            zlogProb = model.prior.logProbability(z, T=T)
-            samples, logDet = model.forward(z, T=T)
-            logProb = zlogProb - logDet
-            energyWithT = xy.energyWithT(samples, T=T)
-            logZ = -(logProb + energyWithT)
-
-            dlogP = torch.autograd.grad(logProb, beta, grad_outputs=torch.ones(batchSize, 1).to(device), create_graph=True, retain_graph=True)[0]
-            dEdT = torch.autograd.grad(energyWithT, beta, grad_outputs=torch.ones(batchSize, 1).to(device), create_graph=True, retain_graph=True)[0]
-
-            dlogZ = -dlogP - dEdT
-
-            if autodiffLevel == 0:
-                logQdivR = model.prior.sourceList[1].logAceptRej()
-
-                dlogQdivR = torch.autograd.grad(logQdivR, beta, grad_outputs=torch.ones(batchSize, 1).to(device), create_graph=True)[0]
-
-                with torch.no_grad():
-                    B2 = (logZ * (dlogQdivR**2)).mean() / (dlogQdivR**2).mean()
-                    B3 = (logProb * (dlogQdivR**2)).mean() / (dlogQdivR**2).mean()
-
-                    partFn = logZ.mean()
-                    partFnErr = logZ.std()
-                    meanE = -(dlogZ + (logZ - B2) * dlogQdivR).mean()
-                    dlogPest = (dlogP + (logProb - B3) * dlogQdivR).mean()
-
-            if autodiffLevel == 1:
-                dzlogProb = torch.autograd.grad(zlogProb, beta, grad_outputs=torch.ones(batchSize, 1).to(device), create_graph=True, retain_graph=True)[0]
-
-                with torch.no_grad():
-                    B2 = (logZ * (dzlogProb**2)).mean() / (dzlogProb**2).mean()
-                    B3 = (logProb * (dzlogProb**2)).mean() / (dzlogProb**2).mean()
-
-                    partFn = logZ.mean()
-                    partFnErr = logZ.std()
-                    meanE = -(dlogZ + (logZ - B2) * dzlogProb).mean()
-                    dlogPest = (dlogP + (logProb - B3) * dzlogProb).mean()
-
-            lnZlst.append(partFn.item()/L**2)
-            lnZerrlst.append(partFnErr.item()/L**2)
-            meanElst.append(meanE.item()/L**2)
-
-            dlogPlst.append(dlogP.mean().item()/L**2)
-            heatClst.append((beta[0] * dlogPest).item()/L**2)
-
-        elif autodiff:
-            beta = beta.detach().requires_grad_(True).to(device)
-
-            z = model.prior.sample(batchSize, T=1/beta)
-            z = z.detach()
-            zlogProb = model.prior.logProbability(z, T=1/beta)
-            samples, logDet = model.forward(z, T=1/beta)
-            logProb = zlogProb - logDet
-            energyWithT = xy.energyWithT(samples, T=1/beta)
-            energy = xy.energyWithT(samples)
-
-            dlogP = torch.autograd.grad(logProb.mean(), beta, create_graph=True, retain_graph=True)[0]
-
-            dEdT = torch.autograd.grad(energyWithT.mean(), beta, create_graph=True, retain_graph=True)[0]
-
-            loss = logProb + energyWithT
-            logZ = -(loss).mean()
-            dlogZ = -dlogP - dEdT
-
-            deltaMean = -dlogZ - energy.mean()
-
-            with torch.no_grad():
-                partFn = logZ
-                partFnErr = loss.std()
-                meanE = -dlogZ
-
-            lnZlst.append(partFn.item()/L**2)
-            lnZerrlst.append(partFnErr.item()/L**2)
-            meanElst.append(meanE.item()/L**2)
-            deltaMeanlst.append(deltaMean.item()/L**2)
-
-            dlogPlst.append(dlogP.item()/L**2)
-            heatClst.append((beta * dlogP.mean()).item()/L**2)
-
-        else:
-            # direct sample and statistical
-            beta = beta.detach()
-            T = 1/beta
-
-            with torch.no_grad():
-                z = model.prior.sample(batchSize, T=T)
+                z = model.prior.sample(smallBatch, T=T)
+                if autodiffLevel == 1:
+                    z = z.detach()
                 zlogProb = model.prior.logProbability(z, T=T)
                 samples, logDet = model.forward(z, T=T)
                 logProb = zlogProb - logDet
-                energy = xy.energy(samples)
-                logZ = -(logProb + energy * beta)
-                partFnErr = logZ.std()
-                logZ = logZ.mean()
-            meanE = energy.mean() / L**2
-            Cv = beta**2 * ((energy**2).mean() - (energy.mean())**2) / L**2
+                energyWithT = xy.energyWithT(samples, T=T)
+                logZ = -(logProb + energyWithT)
 
-            lnZlst.append(logZ.item()/L**2)
-            lnZerrlst.append(partFnErr.item()/L**2)
-            meanElst.append(meanE.item())
-            heatClst.append(Cv.item())
+                dlogP = torch.autograd.grad(logProb, beta, grad_outputs=torch.ones(smallBatch, 1).to(device), create_graph=True, retain_graph=True)[0]
+                dEdT = torch.autograd.grad(energyWithT, beta, grad_outputs=torch.ones(smallBatch, 1).to(device), create_graph=True, retain_graph=True)[0]
 
-    lnZlst = np.array(lnZlst)
-    meanElst = np.array(meanElst)
-    heatClst = np.array(heatClst)
+                dlogZ = -dlogP - dEdT
+
+                if autodiffLevel == 0:
+                    logQdivR = model.prior.sourceList[1].logAceptRej()
+
+                    dlogQdivR = torch.autograd.grad(logQdivR, beta, grad_outputs=torch.ones(smallBatch, 1).to(device), create_graph=True)[0]
+
+                    with torch.no_grad():
+                        B2 = (logZ * (dlogQdivR**2)).mean() / (dlogQdivR**2).mean()
+                        B3 = (logProb * (dlogQdivR**2)).mean() / (dlogQdivR**2).mean()
+
+                        partFn = logZ.mean()
+                        partFnErr = logZ.std()
+                        meanE = -(dlogZ + (logZ - B2) * dlogQdivR).mean()
+                        dlogPest = (dlogP + (logProb - B3) * dlogQdivR).mean()
+
+                if autodiffLevel == 1:
+                    dzlogProb = torch.autograd.grad(zlogProb, beta, grad_outputs=torch.ones(smallBatch, 1).to(device), create_graph=True, retain_graph=True)[0]
+
+                    with torch.no_grad():
+                        B2 = (logZ * (dzlogProb**2)).mean() / (dzlogProb**2).mean()
+                        B3 = (logProb * (dzlogProb**2)).mean() / (dzlogProb**2).mean()
+
+                        partFn = logZ.mean()
+                        partFnErr = logZ.std()
+                        meanE = -(dlogZ + (logZ - B2) * dzlogProb).mean()
+                        dlogPest = (dlogP + (logProb - B3) * dzlogProb).mean()
+
+                lnZlst.append(partFn.item()/L**2)
+                lnZerrlst.append(partFnErr.item()/L**2)
+                meanElst.append(meanE.item()/L**2)
+
+                dlogPlst.append(dlogP.mean().item()/L**2)
+                heatClst.append((beta[0] * dlogPest).item()/L**2)
+
+            elif autodiff:
+                beta = beta.detach().requires_grad_(True).to(device)
+
+                z = model.prior.sample(smallBatch, T=1/beta)
+                z = z.detach()
+                zlogProb = model.prior.logProbability(z, T=1/beta)
+                samples, logDet = model.forward(z, T=1/beta)
+                logProb = zlogProb - logDet
+                energyWithT = xy.energyWithT(samples, T=1/beta)
+                energy = xy.energyWithT(samples)
+
+                dlogP = torch.autograd.grad(logProb.mean(), beta, create_graph=True, retain_graph=True)[0]
+
+                dEdT = torch.autograd.grad(energyWithT.mean(), beta, create_graph=True, retain_graph=True)[0]
+
+                loss = logProb + energyWithT
+                logZ = -(loss).mean()
+                dlogZ = -dlogP - dEdT
+
+                deltaMean = -dlogZ - energy.mean()
+
+                with torch.no_grad():
+                    partFn = logZ
+                    partFnErr = loss.std()
+                    meanE = -dlogZ
+
+                lnZlst.append(partFn.item()/L**2)
+                lnZerrlst.append(partFnErr.item()/L**2)
+                meanElst.append(meanE.item()/L**2)
+                deltaMeanlst.append(deltaMean.item()/L**2)
+
+                dlogPlst.append(dlogP.item()/L**2)
+                heatClst.append((beta * dlogP.mean()).item()/L**2)
+
+            else:
+                # direct sample and statistical
+                beta = beta.detach()
+                T = 1/beta
+
+                with torch.no_grad():
+                    z = model.prior.sample(smallBatch, T=T)
+                    zlogProb = model.prior.logProbability(z, T=T)
+                    samples, logDet = model.forward(z, T=T)
+                    logProb = zlogProb - logDet
+                    energy = xy.energy(samples)
+                    logZ = -(logProb + energy * beta)
+                    partFnErr = logZ.std()
+                    logZ = logZ.mean()
+                meanE = energy.mean() / L**2
+                Cv = beta**2 * ((energy**2).mean() - (energy.mean())**2) / L**2
+
+                lnZlst.append(logZ.item()/L**2)
+                lnZerrlst.append(partFnErr.item()/L**2)
+                meanElst.append(meanE.item())
+                heatClst.append(Cv.item())
+
+    lnZlst = np.array(lnZlst).reshape(stepNum, smallBatchNum).mean(-1)
+    meanElst = np.array(meanElst).reshape(stepNum, smallBatchNum).mean(-1)
+    heatClst = np.array(heatClst).reshape(stepNum, smallBatchNum).mean(-1)
 
     if autodiff or repAcptRej:
-        dlogPlst = np.array(dlogPlst)
+        dlogPlst = np.array(dlogPlst).reshape(stepNum, smallBatchNum).mean(-1)
 
-    lnZerrlst = np.array(lnZerrlst)
+    lnZerrlst = np.array(lnZerrlst).reshape(stepNum, smallBatchNum).mean(-1)
     if autodiff:
-        deltaMeanlst = np.array(deltaMeanlst)
+        deltaMeanlst = np.array(deltaMeanlst).reshape(stepNum, smallBatchNum).mean(-1)
 
     betaLst = betaLst.cpu().numpy()
 
@@ -212,6 +217,7 @@ if __name__ == "__main__":
     L = 16  # lattice length
     num = 300 # plot points number
     batchSize = 1000 # the batch size to estimate free energy
+    smallBatchNum = 10 # num of runs to get the full batch
 
     device = torch.device("cpu")
     #device = torch.device("cuda:1")
